e50a3ea749eecb10d307e1ef23d371bb13ea41dd
Rafael González Martín <rafug95@gmail.com>
2016-11-22 14:00:49 +0100

    añadidos impresiones para depuración

--- unificacion.lisp
+++ unificacion.lisp
@@ -1,3 +1,10 @@
+; Pablo Espinosa Bermejo
+; Rafael González Martín
+; © 2016: Todos los derechos reservados
+
+; Prueba:
+; (unificacion '(P '(? x) '((? g) '(? x))) '(P A '(? z)))
+
 ; Función principal que captura excepciones
 (defun unificacion (e1 e2)
     (catch 'unificacionException (unificar e1 e2))
@@ -22,7 +29,7 @@
 
 ; Función que hace cosas siendo e1 atomo
 (defun anadir (e1 e2)
-    (format t "E1=~a, E2=~b" e1 e2)
+    (format t "Anadir: E1=~a, E2=~b~%" e1 e2)
     (unless (equalp (valorVariable e1) (valorVariable e2)) ; Si e1=e2 no hacer nada
         (if (esVariable e1)
             (if (member e1 e2) ; Member es recursivo??
@@ -37,6 +44,7 @@
     )
 )
 
+; Función que extrae el valor de una variable o devuelve el valor de un átomo
 (defun valorVariable (variable)
     (if (esVariable variable)
       (first(rest variable))
@@ -53,35 +61,50 @@
         ;     (throw 'unificacionException 'Dos-atomos-No-Unificable)
         ; )
         ((atomo e1)
-            (anadir e1 e2)
+            (progn
+                (format t "Atomo e1? E1=~a, E2=~b~%" e1 e2)
+                (anadir e1 e2)
+            )
         )
         ((atomo e2)
-            (anadir e2 e1)
+            (progn
+                (format t "Atomo e2? E1=~a, E2=~b~%" e1 e2)
+                (anadir e2 e1)
+            )
         )
         (t
             ; e1 y e2 son listas
-            (format t "Linea 12")
+            (format t "Linea 12~%")
             (let ((f1 (first e1))
                   (t1 (rest e1))
                   (f2 (first e2))
                   (t2 (rest e2)))
                 (setf z1 (unificar f1 f2))
+                (format t "z1 = ~a~%" z1)
                 (if (equalp z1 'fallo)
-                    (return-from unificar 'fallo)
+                    (progn
+                        (format t "Falla aqui~%")
+                        (return-from unificar 'fallo)
+                    )
                 )
-                (setf g1 (aplicar z t1))
-                (format t "g1 = ~g" g1)
-                (setf g2 (aplicar z t2))
-                (format t "g2 = ~g" g2)
+                (setf g1 (aplicar z1 t1))
+                (format t "g1 = ~g~%" g1)
+                (setf g2 (aplicar z1 t2))
+                (format t "g2 = ~g~%" g2)
                 (setf z2 (unificar g1 g2))
+                (format t "z2 = ~a~%" z2)
                 (if (equalp z2 'fallo)
                     (return-from unificar 'fallo)
                 )
+                (setf composicion (list z1 z2))
+                (format t "Composicion=~c~%" composicion)
+                (return-from unificar composicion)
             )
         )
     )
 )
 
+; Función que sustituye en lista siguiendo lo indicado en expresion
 (defun aplicar (expresion lista)
     (cond
         ((null expresion) lista)
